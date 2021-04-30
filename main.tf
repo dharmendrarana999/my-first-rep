13767964e7c173a909564c4e8f0eb4c29bdd87a1
dharmendrarana999 <80970241+dharmendrarana999@users.noreply.github.com>
2021-04-30 08:53:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,62 @@
-resource "aws_instance" "myweb" {
-  ami           = "ami-05d72852800cbf29e"
-  instance_type = "t2.micro"
-
+resource "aws_instance" "DKRterraform" {
+  ami             = "ami-05d72852800cbf29e"
+  instance_type   = "t2.micro"
+  key_name        = "DharmendraRana"
+  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
   tags = {
-    Name = "terraformDharmendra"
+       Name  = "DKRterraform"
+   }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+ 
+
+  
+   
+    connection {
+      host = "${aws_instance.DKRterraform.public_ip}"
+      type     = "ssh"
+      user     = "ec2-user"
+      private_key = "${file("DharmendraRana.pem")}"
+      
+    }
+  
+
+ 
+
+}
+
+ 
+
+resource "aws_security_group" "instance" {
+  name = "DKR-sg"
+  description = "Allow traffic for instances"
+
+ 
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+ 
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 95
+    to_port     = 95
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ 
+
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
